f0679fa4ca1054acf0c3a13b67f7a00b663c2809
liyangzhen <liyangzhen@qjdchina.com>
2021-01-29 16:25:27 +0800

    增加mybatisplus

---
+++ common-dao-plus/src/main/java/com/liyz/dubbo/common/dao/config/MybatisPlusAutoConfig.java
@@ -0,0 +1,49 @@
+package com.liyz.dubbo.common.dao.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 注释:
+ *
+ * @author liyangzhen
+ * @version 1.0.0
+ * @date 2021/1/29 16:18
+ */
+@Configuration
+public class MybatisPlusAutoConfig {
+
+/*    *//**
+     * 添加分页插件
+     *
+     * @return
+     *//*
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        //分页插件
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }*/
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInnerInterceptor paginationInterceptor() {
+        return new PaginationInnerInterceptor();
+    }
+
+    /**
+     * 乐观锁插件:实体字段带@Version则有乐观锁功能
+     */
+    @Bean
+    public OptimisticLockerInnerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInnerInterceptor();
+    }
+}
---
+++ common-dao-plus/src/main/java/com/liyz/dubbo/common/dao/handler/MybatisPlusMetaObjectHandler.java
@@ -0,0 +1,34 @@
+package com.liyz.dubbo.common.dao.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
+
+/**
+ * 注释:
+ *
+ * @author liyangzhen
+ * @version 1.0.0
+ * @date 2021/1/29 16:01
+ */
+@Slf4j
+@Configuration
+public class MybatisPlusMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("MybatisPlusMetaObjectHandler init ...");
+        Date now = new Date();
+        setFieldValByName("create_time", now, metaObject);
+        setFieldValByName("update_time", now, metaObject);
+        setFieldValByName("isDeleted", 0, metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        setFieldValByName("update_time", new Date(), metaObject);
+    }
+}
