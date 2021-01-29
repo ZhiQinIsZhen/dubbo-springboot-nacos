94a8ee856ac8bb2020a4ff071cc16ea98ade1b8e
liyangzhen <liyangzhen@qjdchina.com>
2021-01-29 16:42:46 +0800

    增加test service

---
+++ service-test/test-provider/src/main/java/com/liyz/dubbo/service/test/TestApplication.java
@@ -0,0 +1,23 @@
+package com.liyz.dubbo.service.test;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * 注释:启动类
+ *
+ * @author liyangzhen
+ * @version 1.0.0
+ * @date 2021/1/29 16:32
+ */
+@EnableCaching
+@EnableDubbo(scanBasePackages = {"com.liyz.dubbo.service.test.provider"})
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
