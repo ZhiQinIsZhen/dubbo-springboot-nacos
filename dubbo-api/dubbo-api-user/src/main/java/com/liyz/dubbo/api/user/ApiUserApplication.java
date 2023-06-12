a74b0031f9f6e2949a7afcad34a83b425b5a9f76
liyangzhen <812672598@qq.com>
2023-06-12 20:44:10 +0800

    add module: dubbo-api-user

---
+++ dubbo-api/dubbo-api-user/src/main/java/com/liyz/dubbo/api/user/ApiUserApplication.java
@@ -0,0 +1,19 @@
+package com.liyz.dubbo.api.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Desc:启动类
+ *
+ * @author lyz
+ * @version 1.0.0
+ * @date 2023/6/12 16:48
+ */
+@SpringBootApplication
+public class ApiUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUserApplication.class, args);
+    }
+}
